7bf4f7b9b86a3662ed5ea643519187bb5a2772c6
Linah Wainaina <wainainalinahw@gmail.com>
2023-11-20 10:27:41 +0300

    Installed elixir

--- lib/elixir60_days.ex
+++ lib/elixir60_days.ex
@@ -1,19 +1,3 @@
 defmodule Elixir60Days do
-  @moduledoc """
-  Documentation for `Elixir60Days`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Elixir60Days.hello()
-      :world
-
-  """
-  def hello do
-
-    :world
   end
 end
